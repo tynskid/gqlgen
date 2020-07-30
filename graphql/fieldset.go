7457168ce847d0b1972f30c75b2d4fbad4814a4b
tynskid <67418614+tynskid@users.noreply.github.com>
2020-07-30 18:23:17 -0400

    Update fieldset.go

--- graphql/fieldset.go
+++ graphql/fieldset.go
@@ -28,26 +28,11 @@ func (m *FieldSet) Concurrently(i int, f func() Marshaler) {
 }
 
 func (m *FieldSet) Dispatch() {
-	if len(m.delayed) == 1 {
 		// only one concurrent task, no need to spawn a goroutine or deal create waitgroups
 		d := m.delayed[0]
 		m.Values[d.i] = d.f()
 		fmt.Println('one')
-	} else if len(m.delayed) > 1 {
-		// more than one concurrent task, use the main goroutine to do one, only spawn goroutines for the others
-	fmt.Println('two')
-		var wg sync.WaitGroup
-		for _, d := range m.delayed[1:] {
-			wg.Add(1)
-			go func(d delayedResult) {
-				m.Values[d.i] = d.f()
-				wg.Done()
-			}(d)
-		}
 
-		m.Values[m.delayed[0].i] = m.delayed[0].f()
-		wg.Wait()
-	}
 }
 
 func (m *FieldSet) MarshalGQL(writer io.Writer) {
